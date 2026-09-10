d7828bf357cfaf51c9516a8343010ee9709d3b7d
Mohammed Yasar M <mohammedyasar990@gmail.com>
2026-09-10 14:43:15 +0530

    Change photo URL for Mohammed Sufyaan
    Updated the photo URL for Mohammed Sufyaan in the team members list.

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -77,7 +77,7 @@ const teams = [
       "The amplifiers of the web, carrying the signal of REVIBE '26 across every channel.",
     members: [
       { name: "Mohammed Abuzar", dept: "IV Year CSE", role: "Social Media Lead", lead: true, photo: "https://res.cloudinary.com/dbqjkjl0c/image/upload/v1788170104/ABUZPIC2_elgeqj.jpg" },
-      { name: "Mohammed Sufyaan", dept: "II Year IT", photo:"https://res.cloudinary.com/djm8qhle1/image/upload/v1788339110/1000161921_-_Mohammed_Sufyaan_Z_lndfii.png"},
+      { name: "Mohammed Sufyaan", dept: "II Year IT", photo:"https://res.cloudinary.com/djm8qhle1/image/upload/v1789031179/IMG-20260910-WA0004_pyzxpe.jpg"},
       { name: "S Mohammed Saad", dept: "II Year AI-DS", photo: "https://res.cloudinary.com/djm8qhle1/image/upload/v1787935208/S_MOHAMMED_SAAD_h03juj.png" },
       { name: "Mohammed Fahad Khan FM", dept: "II Year IT", photo: "https://res.cloudinary.com/djm8qhle1/image/upload/v1787935189/Fahad_Khan_gtft9u.png" },
       { name: "A Muhammed Saad", dept: "II Year IT", photo: "https://res.cloudinary.com/djm8qhle1/image/upload/v1787935198/A_Muhammad_Saad_dqvnob.jpg " },
